4c38b1fa8219c56b01c3755b8858ef95c0214a5d
cclontz <clontz.chris@gmail.com>
2020-01-02 20:23:10 -0600

    Add logic to cancel process if errors are encountered. That way error handling can be implemented as they are encountered through use and testing.

--- src/streaming/fileProcessor.ts
+++ src/streaming/fileProcessor.ts
@@ -5,39 +5,44 @@ import { IProcessorOptions } from '../types';
 import chunkProcessor from './chunkProcessor';
 
 function fileProcessor(srcFile: File, opts: IProcessorOptions) {
-	const downStream: WritableStream = streamSaver.createWriteStream(
-		'testOne.csv'
-	);
-	const streamWriter = downStream.getWriter();
-	const textEncoder = new TextEncoder();
+  const downStream: WritableStream = streamSaver.createWriteStream(
+    'testOne.csv'
+  );
+  const streamWriter = downStream.getWriter();
+  const textEncoder = new TextEncoder();
 
-	parse(srcFile, {
-		error(err) {
-			// Log error on reader
-			console.error(err);
-		},
-		async chunk(results, parser) {
-			const { data, errors, meta } = results;
-			parser.pause();
-			await streamWriter.ready;
-			// Process chunk
-			const entityArr = chunkProcessor(data, opts);
-
-			const unparsed = unparse(entityArr);
-			const encoded = textEncoder.encode(unparsed);
-
-			try {
-				await streamWriter.write(encoded);
-				parser.resume();
-			} catch (e) {
-				console.error(e);
-			}
-		},
-		async complete() {
-			await streamWriter.ready;
-			streamWriter.close();
-		}
-	});
+  parse(srcFile, {
+    error(err) {
+      // Log error on reader
+      console.error(err);
+    },
+    async chunk(results, parser) {
+      const { data, errors, meta } = results;
+      parser.pause();
+      await streamWriter.ready;
+      // Process chunk
+      try {
+        // Process chunk
+        const entityArr = chunkProcessor(data, opts);
+        // Convert back to csv
+        const unparsed = unparse(entityArr);
+        // Encode csv string
+        const encoded = textEncoder.encode(unparsed);
+        // Write to stream
+        await streamWriter.write(encoded);
+        // Continue parsing
+        parser.resume();
+      } catch (e) {
+        parser.abort();
+        streamWriter.abort();
+        console.error(e);
+      }
+    },
+    async complete() {
+      await streamWriter.ready;
+      streamWriter.close();
+    }
+  });
 }
 
 export default fileProcessor;
